239fa7550c48b3173a5f5a06dd1d0a6898695a64
adarshjos <adarshjoseph97@gmail.com>
2017-09-07 12:16:04 +0530

    try

--- bot.py
+++ bot.py
@@ -2,31 +2,28 @@ import sys
 import sqlite3
 import time
 import telepot
+import  json 
+import cStringIO
+
+from PIL import Image
+from pytesseract import image_to_string
 from telepot.loop import MessageLoop
-import  json
 from urllib2 import urlopen
-url = 'https://api.telegram.org/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/getFile?file_id='
 
 
-conn = sqlite3.connect('eappan.db')
 bot = telepot.Bot('429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0')
+conn = sqlite3.connect('eappan.db',check_same_thread=False)
+
 def handle(msg):
 	
 		content_type, chat_type, chat_id = telepot.glance(msg)
 		print(content_type, chat_type, chat_id)
-		if content_type == 'text':
-			if msg['text'] in ['hello','hi']:
-		 
-				bot.sendMessage(chat_id, msg['text'])
-					
-			else:
-				bot.sendMessage(chat_id, "Start With Hi or Hello to talk with me!!")
-		else:
-				if content_type == 'document':
+		if content_type == 'photo' :
+					print 'photo'
 					# bot.sendDocument(chat_id,msg['document']['file_id'])
-					# print (msg['document']['file_size'])
-					# print (msg['document']['file_id'])
-					idfile=msg['document']['file_id']
+					# print (msg)
+					# print (msg['photo'][3]['file_id'])
+					idfile=msg['photo'][3]['file_id']
 					url = 'https://api.telegram.org/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/getFile?file_id='
 					url=url+str(idfile)
 					print url
@@ -35,6 +32,13 @@ def handle(msg):
 					print(data)
 					url1='https://api.telegram.org/file/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/'+data['result']['file_path']
 					print(url1)
+					file = cStringIO.StringIO(urlopen(url1).read())
+					c=image_to_string(Image.open(file))
+					l=c.split()
+					bot.sendMessage(chat_id, l)
+					conn.execute("INSERT INTO botocr (fileid,ocrdata) VALUES (?,?)",(str(idfile),c));
+					print("databse entered")
+	
 
 					
 
