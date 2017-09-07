b2c6b3740af6689680a1d64c796cd2e3ecaf9a03
adarshjos <adarshjoseph97@gmail.com>
2017-09-07 22:21:33 +0530

    final

--- bot.py
+++ bot.py
@@ -12,35 +12,45 @@ from urllib2 import urlopen
 
 
 bot = telepot.Bot('429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0')
-conn = sqlite3.connect('eappan.db',check_same_thread=False)
-
+conne = sqlite3.connect('/home/adarsh/academics/Bot/eappan.db',check_same_thread=False)
+# conn.execute("CREATE TABLE botocr(fileid varchar(1000),ocrdata varchar(2000))")
+conn = conne.cursor()
 def handle(msg):
+
 	
 		content_type, chat_type, chat_id = telepot.glance(msg)
 		print(content_type, chat_type, chat_id)
 		if content_type == 'photo' :
-					print 'photo'
-					# bot.sendDocument(chat_id,msg['document']['file_id'])
-					# print (msg)
-					# print (msg['photo'][3]['file_id'])
-					idfile=msg['photo'][3]['file_id']
-					url = 'https://api.telegram.org/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/getFile?file_id='
-					url=url+str(idfile)
-					print url
-					response = urlopen(url);
-					data = json.loads(response.read())
-					print(data)
-					url1='https://api.telegram.org/file/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/'+data['result']['file_path']
-					print(url1)
-					file = cStringIO.StringIO(urlopen(url1).read())
-					c=image_to_string(Image.open(file))
-					l=c.split()
-					bot.sendMessage(chat_id, l)
-					conn.execute("INSERT INTO botocr (fileid,ocrdata) VALUES (?,?)",(str(idfile),c));
-					print("databse entered")
-	
 
-					
+			print 'photo'
+			idfile=msg['photo'][3]['file_id']
+			#bot.sendPhoto(chat_id,idfile)
+			url = 'https://api.telegram.org/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/getFile?file_id='
+			url=url+str(idfile)
+			print url
+			response = urlopen(url);
+			data = json.loads(response.read())
+			print(data)
+			url1='https://api.telegram.org/file/bot429545323:AAEj3Pqo3ItuNWGefbJjy4N-lwrqzOseZD0/'+data['result']['file_path']
+			print(url1)
+			file = cStringIO.StringIO(urlopen(url1).read())
+			c=image_to_string(Image.open(file))
+			l=c.split()
+			#bot.sendMessage(chat_id, l)
+			conn.execute("INSERT INTO botocr (fileid,ocrdata) VALUES (?,?)",((idfile),c));
+			print("databse entered")
+			conne.commit()
+		elif content_type == 'text' :
+			print msg
+			print (msg['text'])
+			find='%'+msg['text']+'%'
+			print find
+			conn.execute("SELECT fileid FROM botocr WHERE ocrdata LIKE ?",(find,));
+			final=conn.fetchone()	
+			print final[0]
+			print type(final)	
+			bot.sendPhoto(chat_id,final[0])
+ 
 
 
 
